cd82f31432336ef0fe535c2d509d9faa454f018a
Haltec Sandi <sandi.haltec@gmail.com>
2023-05-16 12:36:15 +0700

    fix header

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ const authStore = useAuthStore()
 </script>
 
 <template>
-  <header>
+  <header v-if="authStore?.user?.name">
     <div class="wrapper">
       <nav class="bg-white border-gray-200 dark:bg-gray-900">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
